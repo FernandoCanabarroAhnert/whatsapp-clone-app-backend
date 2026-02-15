119ff205fbe3b16693390e55c4af001e7a593493
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2026-02-15 19:57:19 -0300

    init: projeto inicializado

---
+++ src/main/java/com/fernandocanabarro/whatsapp_clone_app_backend/WhatsappCloneAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.whatsapp_clone_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappCloneAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/whatsapp_clone_app_backend/WhatsappCloneAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.whatsapp_clone_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
